3c07eb5d82a288e2e20afea70c3650bf25c5a1de
Richard Peterson <richardpeterson@Bryans-iMac-2.local>
2016-04-15 16:44:40 -0500

    added admin front end student page

--- src/demo/demo-angular.js
+++ src/demo/demo-angular.js
@@ -1,9 +1,37 @@
-angular.module('demoSortApp', [])
-
-.controller('usersCtrl', function($scope, $http) {
+var userModel = angular.module('userModel', []);
+userModel.controller('usersCtrl', function($scope, $http) {
   $http.get('./src/demo/demo-users.php').
   	success(function(data){
   		$scope.users = data;
   	});
   
 });
+
+
+var studentModel = angular.module('studentModel', [])
+studentModel.controller('studentsCtrl', function($scope, $http) {
+    
+  $scope.showData = function(){
+        $scope.curPage = 0;
+        $scope.pageSize = 15;
+
+        $http.get('./src/demo/demo-students.php').
+        success(function(data){
+          $scope.students = data;
+        });
+
+        $scope.numberOfPages = function(){
+          return Math.ceil($scope.data.length / $scope.pageSize);
+        }
+
+  }  
+  
+  
+});
+
+
+
+
+
+
+
